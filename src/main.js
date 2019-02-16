9f20288d294d2e9fc5ba0c5d7d516551f633910d
gaoguanbao <757611909@qq.com>
2019-02-16 10:37:18 +0800

    用户登录功能实现完毕

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入global.css全局样式控制文件
+import './assets/css/global.css'
+
+// 引入 图标 css样式文件
+import './assets/fonts/iconfont.css'
+
+// 引入element-ui组件库模块
+import ElementUI from 'element-ui'
+
+// 引入axios并做相关配置
+import axios from 'axios'
+// axios请求根地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 给axios配置给Vue的$http成员
+Vue.prototype.$http = axios
+
+// 把element-ui 注册给Vue
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
